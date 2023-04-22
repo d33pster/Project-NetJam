5186866678bc853b1448f531d968179eb8eb256c
d33pster <deep.main.ac@gmail.com>
2023-04-22 06:09:25 +0530

    Added Driver Program but still not operable and Under Maintainance

---
+++ main/driver.sh
@@ -0,0 +1,59 @@
+#!/bin/sh
+echo "
+
+ ███▄    █ ▓█████▄▄▄█████▓    ▄▄▄██▀▀▀▄▄▄       ███▄ ▄███▓
+ ██ ▀█   █ ▓█   ▀▓  ██▒ ▓▒      ▒██  ▒████▄    ▓██▒▀█▀ ██▒
+▓██  ▀█ ██▒▒███  ▒ ▓██░ ▒░      ░██  ▒██  ▀█▄  ▓██    ▓██░
+▓██▒  ▐▌██▒▒▓█  ▄░ ▓██▓ ░    ▓██▄██▓ ░██▄▄▄▄██ ▒██    ▒██ 
+▒██░   ▓██░░▒████▒ ▒██▒ ░     ▓███▒   ▓█   ▓██▒▒██▒   ░██▒
+░ ▒░   ▒ ▒ ░░ ▒░ ░ ▒ ░░       ▒▓▒▒░   ▒▒   ▓▒█░░ ▒░   ░  ░
+░ ░░   ░ ▒░ ░ ░  ░   ░        ▒ ░▒░    ▒   ▒▒ ░░  ░      ░
+   ░   ░ ░    ░    ░          ░ ░ ░    ░   ▒   ░      ░   
+         ░    ░  ░            ░   ░        ░  ░       ░   
+                                                          
+"
+echo "Version 1.0"
+
+echo ""
+echo "1. Fluxion"
+echo ""
+
+
+read -p "Enter Choice: " choice
+echo ""
+
+
+#Functions
+fluxion() {
+      chmod +x /home/heck/fluxion/fluxion.sh
+      /home/heck/fluxion/fluxion.sh -l english
+}
+
+
+#conditional
+if [ "$choice" = 1 ]
+then
+airmon-ng check kill
+airmon-ng start wlan0mon
+fluxion
+service wpa_supplicant restart
+service NetworkManager restart
+else
+echo "error"
+fi
+
+
+
+service wpa_supplicant restart
+service NetworkManager restart
+
+
+
+
+
+
+
+
+
+ 
+
